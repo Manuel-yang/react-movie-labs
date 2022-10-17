6fb3f775f5541d80bdb2d98936cdd7bd45c35856
Yimeng Yang <731270812@qq.com>
2022-10-17 11:23:06 +0100

    Added Upcoming movies page.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
+import UpcomingMovies from './pages/upcomingMovies'
 
 
 // const images = [
@@ -106,6 +107,7 @@ const App = () => {
         <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
         <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/movies/upcoming" element={<UpcomingMovies />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
       </Routes>
