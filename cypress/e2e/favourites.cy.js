3bb2322948df5ef26a3c2923e3e176ae62c0d30f
Yimeng Yang <731270812@qq.com>
2022-10-18 12:26:56 +0100

    E2E tests - Favourites feature tested.

--- cypress/e2e/favourites.cy.js
+++ cypress/e2e/favourites.cy.js
@@ -1,7 +1,7 @@
 let movies;
-let movieId; // Enola Holmes movie id
+const movieId = 497582; // Enola Holmes movie id
 
-describe("Navigation", () => {
+describe("The favourites feature", () => {
   before(() => {
     cy.request(
       `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
@@ -16,50 +16,32 @@ describe("Navigation", () => {
   beforeEach(() => {
     cy.visit("/");
   });
-  describe("From the home page to a movie's details", () => {
-    it("navigates to the movie details page and change browser URL", () => {
-      cy.get(".MuiCardActions-root").eq(0).contains("More Info").click();
-      cy.url().should("include", `/movies/${movies[0].id}`);
+
+  describe("Selecting favourites", () => {
+    it("selected movie card shows the red heart", () => {
+      cy.get(".MuiCardHeader-root").eq(1).find("svg").should("not.exist");
+      cy.get("button[aria-label='add to favorites']").eq(1).click();
+      cy.get(".MuiCardHeader-root").eq(1).find("svg");
     });
   });
-  describe("The site header", () => {
-    describe("when the viewport is desktop scale", () => {
-      it("navigation via the links", () => {
-        cy.get("button").contains("Favorites").click();
-        cy.url().should("include", `/favorites`);
-        cy.get("button").contains("Home").click();
-        cy.url().should("include", `/`);
-      });
-    });
-    describe(
-      "when the viewport is a mobile scale",
-      {
-        viewportHeight: 896,
-        viewportWidth: 414,
-      },
-      () => {
-        it("navigation via the dropdown menu", () => {
-          cy.get("header").find("button").click();
-          cy.get("li").contains('Favorites').click();
-          cy.url().should("include", `/favorites`);
-          cy.get("li").contains('Home').click();
-          cy.url().should("include", `/`);
-        });
-      }
-    );
-  });
-  describe("From the favourites page to a movie's details", () => {
-    // TODO
-  });
-  describe("The forward/backward links", () => {
+
+  describe("The favourites page", () => {
     beforeEach(() => {
-      cy.get(".MuiCardActions-root").eq(0).contains("More Info").click();
+      // Select two favourites and navigate to Favourites page
+      cy.get("button[aria-label='add to favorites']").eq(1).click();
+      cy.get("button[aria-label='add to favorites']").eq(3).click();
+      cy.get("button").contains("Favorites").click();
     });
-    it("navigates between the movies detail page and the Home page.", () => {
-      cy.get("svg[data-testid='ArrowBackIcon'").click();
-      cy.url().should("not.include", `/movies/${movies[0].id}`);
-      cy.get("svg[data-testid='ArrowForwardIcon'").click();
-      cy.url().should("include", `/movies/${movies[0].id}`);
+    it("only the tagged movies are listed", () => {
+      cy.get(".MuiCardHeader-content").should("have.length", 2);
+      cy.get(".MuiCardHeader-content")
+        .eq(0)
+        .find("p")
+        .contains(movies[1].title);
+      cy.get(".MuiCardHeader-content")
+        .eq(1)
+        .find("p")
+        .contains(movies[3].title);
     });
   });
 });
