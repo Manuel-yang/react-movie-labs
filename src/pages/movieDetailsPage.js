5adc82dc01e8bb98db6a768275a00701eed04335
Yimeng Yang <731270812@qq.com>
2022-11-20 12:33:24 +0000

    feat : add lazy loading in movieDetails

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, {lazy, Suspense} from "react";
 import { useParams } from 'react-router-dom';
-import MovieDetails from "../components/movieDetails/";
-import PageTemplate from "../components/templateMoviePage";
 // import useMovie from "../hooks/useMovie";
 import { getMovie } from '../api/tmdb-api'
 import { useQuery } from "react-query";
 import Spinner from '../components/spinner'
+const MovieDetails = lazy(() => import("../components/movieDetails/"))
+const PageTemplate = lazy(() => import("../components/templateMoviePage"))
 
 const MoviePage = (props) => {
   const { id } = useParams();
@@ -26,9 +26,11 @@ const MoviePage = (props) => {
     <>
       {movie ? (
         <>
-          <PageTemplate movie={movie}>
-            <MovieDetails movie={movie} />
-          </PageTemplate>
+          <Suspense>
+            <PageTemplate movie={movie}>
+              <MovieDetails movie={movie} />
+            </PageTemplate>
+          </Suspense>
         </>
       ) : (
         <p>Waiting for movie details</p>
