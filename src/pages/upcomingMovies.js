87b30873d8e33853cc42bbbe2ea1b97580713692
Yimeng Yang <731270812@qq.com>
2022-11-20 12:55:09 +0000

    feat : add lazy loading in upcoming movies

--- src/pages/upcomingMovies.js
+++ src/pages/upcomingMovies.js
@@ -1,9 +1,9 @@
-import React from "react";
-import PageTemplate from '../components/templateMovieListPage'
+import React, { lazy, Suspense } from "react";
 import { getUpcomingMovies } from "../api/tmdb-api";
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
-import AddToPlaylistIcon from '../components/cardIcons/addToPlaylist'
+const PageTemplate = lazy(() => import('../components/templateMovieListPage'))
+const AddToPlaylistIcon = lazy(() => import('../components/cardIcons/addToPlaylist'))
 
 const HomePage = () => {
 
@@ -24,13 +24,15 @@ const HomePage = () => {
   // const addToFavorites = (movieId) => true 
 
   return (
-    <PageTemplate
-      title="Upcoming Movies"
-      movies={movies}
-      action={(movie) => {
-        return <AddToPlaylistIcon movie={movie} />
-      }}
-    />
+    <Suspense>
+      <PageTemplate
+        title="Upcoming Movies"
+        movies={movies}
+        action={(movie) => {
+          return <AddToPlaylistIcon movie={movie} />
+        }}
+      />
+    </Suspense>
 );
 };
 export default HomePage;
