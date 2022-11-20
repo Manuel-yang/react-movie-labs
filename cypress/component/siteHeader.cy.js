99d14fd099ed9889b5ff242e0848c29212cc2c97
Yimeng Yang <731270812@qq.com>
2022-11-20 22:42:05 +0000

    fix : fix some bugs in component testing

--- cypress/component/siteHeader.cy.js
+++ cypress/component/siteHeader.cy.js
@@ -2,31 +2,45 @@ import SiteHeader from '../../src/components/siteHeader'
 import { BrowserRouter } from "react-router-dom";
 
 describe('siteHeader.cy.js', () => {
-  beforeEach(() => {
+  it('contain logo name', () => {
     cy.mount(
       <BrowserRouter>
         <SiteHeader/>
       </BrowserRouter>)
-  })
-  it('contain logo name', () => {
     cy.get(".css-p7bec2-MuiTypography-root").contains('YanAemons')
   })
 
   it("can input info in search bar", () => {
+    cy.mount(
+      <BrowserRouter>
+        <SiteHeader/>
+      </BrowserRouter>)
     cy.get("input").type("Black adam")
   })
 
   it("click home button", () => {
+        cy.mount(
+      <BrowserRouter>
+        <SiteHeader/>
+      </BrowserRouter>)
     cy.get("button")
       .contains("Home")
       .click()
   })
 
   it("click home button", () => {
+    cy.mount(
+      <BrowserRouter>
+        <SiteHeader/>
+      </BrowserRouter>)
     cy.get("button").contains("Favourites").click();
   })
 
   it("click home button", () => {
+    cy.mount(
+      <BrowserRouter>
+        <SiteHeader/>
+      </BrowserRouter>)
     cy.get("button").contains("Upcoming").click();
   })
 })
