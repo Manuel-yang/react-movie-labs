80b7d33cdcdbeb8153f543fb633951946a2b5397
Yimeng Yang <731270812@qq.com>
2022-12-27 21:55:26 +0000

    feat : add upload avatar function

--- src/components/userInfoSite/index.js
+++ src/components/userInfoSite/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -11,25 +11,40 @@ import PhotoCamera from '@mui/icons-material/PhotoCamera';
 import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
 import Chip  from "@mui/material/Chip";
-import { Container } from "postcss";
+import { Upload } from "upload-js";
 
 const UserInfoSite = (props) => {
+  const [fileUrl, setFileUrl] = useState("")
+  const [uploadprogress, setProgress] = useState("")
+  const upload = Upload({ apiKey: process.env.REACT_APP_UPLOAD_KEY }); // Your real API key.
+
+  const onFileSelected = async (event) => {
+    const [ file ]    = event.target.files;
+    const { fileUrl } = await upload.uploadFile(file, { onProgress });
+    setProgress("")
+    setFileUrl(fileUrl)
+  }
+  
+  const onProgress = ({ progress }) => {
+    setProgress(progress)
+  }
   return(
     <div>
       <Card sx={{ maxWidth: 345 }}>
         <CardMedia
           component="img"
           alt="avatar"
-          image="https://i.328888.xyz/2022/12/27/UVdOp.jpeg"
+          image={fileUrl? fileUrl: "https://i.328888.xyz/2022/12/27/UVdOp.jpeg"}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {localStorage.getItem("username")}
           </Typography>
         </CardContent>
+        <h2>{uploadprogress? "Uploading "+uploadprogress+"%":""}</h2>
         <CardActions>
           <Stack direction="row" alignItems="center" spacing={2}>
-            <Button variant="contained" component="label">
+            <Button onChange={onFileSelected} variant="contained" component="label">
               Upload
               <input hidden accept="image/*" multiple type="file" />
             </Button>
